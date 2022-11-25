32e50c4133adb632269cfc596a70f10923865ab4
Abbos <you@example.com>
2022-11-25 10:11:00 +0500

    added cars

--- cars/urls.py
+++ cars/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from .views import CarListView, search
+from .views import CarListView
 
 app_name = 'car'
 
 urlpatterns = [
     path('', CarListView.as_view(), name='cars'),
-    path('search/', search, name='search'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,30 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
 from .models import CarModel
-from django.core.paginator import EmptyPage,Paginator, PageNotAnInteger
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 
 
 class CarListView(ListView):
     queryset = CarModel.objects.all()
     template_name = 'cars.html'
-    paginate_by = 2
+    paginate_by = 6
     page_kwarg = 'car'
 
+    def get_queryset(self):
+        qs = CarModel.objects.all()
+
+        search = self.request.GET.get('search')
+        if search:
+            qs = qs.filter(description__icontains=search)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['cars'] = CarModel.objects.all()
 
 
-def search(request):
-    search_cars = CarModel.objects.order_by('-created_at')
-
-    if 'keyword' in request.GET:
-        keyword = request.GET['keyword']
-        if keyword:
-            search_cars = search_cars.filter(description__icontains=keyword)
-
-    data = {
-        'search_cars': search_cars,
-
-    }
-    return render(request, 'search.html', data)
-
-
-
+        return data
